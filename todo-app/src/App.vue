2f22506eb29fcf25e27e93f356a8f0519d40b0c0
priya <g.gomathipriya@gmail.com>
2017-10-02 19:17:30 +0530

    SimplyRenderingTODOList

---
+++ todo-app/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+   <!-- <hello></hello> -->
+   <todo-list v-bind:todos="todos"></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+
+export default {
+  name: 'app',
+  components: {
+    TodoList,
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'TODO A',
+        project: 'Project A',
+        done: 'false',
+      }, {
+        title: 'TODO B',
+        project: 'Project B',
+        done: 'true',
+      }, {
+        title: 'TODO C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'TODO D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
